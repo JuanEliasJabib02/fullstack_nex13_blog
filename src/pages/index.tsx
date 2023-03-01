2b6c2ea368e36fbc05d0d61c8b1ae3a4c4b189b6
JuanEliasJabib02 <juaneliasjabib02@gmail.com>
2023-03-01 16:58:55 -0500

    finish project

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,14 @@
+import { useState } from "react";
 import { type NextPage } from "next";
 import Head from "next/head";
-import Link from "next/link";
-import { signIn, signOut, useSession } from "next-auth/react";
-import { Header } from "~/components/Header";
+import { useSession } from "next-auth/react";
+
 import { api, type RouterOutputs } from "../utils/api";
-import { useState } from "react";
-
-
+import { Header } from "../components/Header";
+import { NoteEditor } from "../components/NoteEditor";
+import { NoteCard } from "../components/NoteCard";
 
 const Home: NextPage = () => {
-
   return (
     <>
       <Head>
@@ -19,7 +18,7 @@ const Home: NextPage = () => {
       </Head>
       <main>
         <Header />
-        < Content />
+        <Content />
       </main>
     </>
   );
@@ -27,27 +26,48 @@ const Home: NextPage = () => {
 
 export default Home;
 
-
-
-
+type Topic = RouterOutputs["topic"]["getAll"][0];
 
 const Content: React.FC = () => {
+  const { data: sessionData } = useSession();
 
-  type Topic = RouterOutputs["topic"]["getAll"][0];
-  const { data: sessionData } = useSession()
   const [selectedTopic, setSelectedTopic] = useState<Topic | null>(null);
 
   const { data: topics, refetch: refetchTopics } = api.topic.getAll.useQuery(
     undefined, // no input
     {
-      enabled: sessionData?.user !== undefined
+      enabled: sessionData?.user !== undefined,
+      onSuccess: (data) => {
+        setSelectedTopic(selectedTopic ?? data[0] ?? null);
+      },
     }
   );
 
   const createTopic = api.topic.create.useMutation({
     onSuccess: () => {
-      void refetchTopics()
+      void refetchTopics();
+    },
+  });
+
+  const { data: notes, refetch: refetchNotes } = api.note.getAll.useQuery(
+    {
+      topicId: selectedTopic?.id ?? "",
+    },
+    {
+      enabled: sessionData?.user !== undefined && selectedTopic !== null,
     }
+  );
+
+  const createNote = api.note.create.useMutation({
+    onSuccess: () => {
+      void refetchNotes();
+    },
+  });
+
+  const deleteNote = api.note.delete.useMutation({
+    onSuccess: () => {
+      void refetchNotes();
+    },
   });
 
   return (
@@ -72,7 +92,7 @@ const Content: React.FC = () => {
         <input
           type="text"
           placeholder="New Topic"
-          className="input-bordered input-sm w-full"
+          className="input-bordered input input-sm w-full"
           onKeyDown={(e) => {
             if (e.key === "Enter") {
               createTopic.mutate({
@@ -81,10 +101,30 @@ const Content: React.FC = () => {
               e.currentTarget.value = "";
             }
           }}
-        >
-        </input>
+        />
       </div>
-      <div className="col-span-3"></div>
+      <div className="col-span-3">
+        <div>
+          {notes?.map((note) => (
+            <div key={note.id} className="mt-5">
+              <NoteCard
+                note={note}
+                onDelete={() => void deleteNote.mutate({ id: note.id })}
+              />
+            </div>
+          ))}
+        </div>
+
+        <NoteEditor
+          onSave={({ title, content }) => {
+            void createNote.mutate({
+              title,
+              content,
+              topicId: selectedTopic?.id ?? "",
+            });
+          }}
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,4 @@
 import { createTRPCRouter } from "~/server/api/trpc";
-import { exampleRouter } from "~/server/api/routers/example";
 import { topicRouter } from "./routers/topic";
 import { noteRouter } from "./routers/notes";
 
@@ -10,7 +9,6 @@ import { noteRouter } from "./routers/notes";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
   topic: topicRouter,
   note: noteRouter,
 });
